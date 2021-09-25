c1f98c61085fb19eece7dbf7a62d1db94e4c12a0
lohade <abhisheklohade8584@gmail.com>
2021-09-25 12:08:02 +0530

    inserting elements in middle

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+
 import java.util.Scanner;
 
 public class LinkedList {
@@ -8,74 +9,88 @@ public class LinkedList {
     static class  Node {
         int data;
         Node next;
-
+        //declare a parameterised constructor
         public Node(int data) {
             this.data =data;
             this.next = null;
         }
     }
-
+    //adding node to linkedlist array
     public void addNode(int data){
-        Node newNode=new Node(data);
-        newNode.data=data;
-        newNode.next=head;
-        head=newNode;
+        Node newnode=new Node(data);
+        newnode.next=null;
+        //checking head is empty or not
+        if(this.head==null){
+            head=newnode;//head is creating as newnode
+        }
+        else{
+            Node last=this.head;//head is assigned as last
+            //checking till last node
+             while(last.next!=null){
+                last=last.next;
+            }
+            last.next=newnode;//last node assigned as new node
+        }
+    }
+    //adding node in middle
+    public void addmiddle(int x){
+        Node newnode=new Node(x);//x is creating as data
+        int length=0;
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        else{
+            Node temp=head;
+            while(temp.next!=null){
+                length++;//calculate total size of linked list
+                temp=temp.next;
+            }
+            int position;
+            if(length%2==0){
+                position=length/2;
+            }
+            else{
+                position=(length+1)/2;
+            }
+            temp = head;
+            while(position-- >1)
+                temp=temp.next;
+            newnode.next=temp.next;
+            temp.next=newnode;
+
+        }
     }
 
-
+    //display function
     public void display(){
-        Node temp=head;
-
         if(head==null){
             System.out.println("empty list");
-            return;
         }
-        while(temp!=null){
-            System.out.print("->"+temp.data+"");
-            temp=temp.next;
-        }
-    }
-    public void reverse_display(){
         Node temp=head;
-        Node prev=null,current;
-
-        while (temp!=null){
-            current=temp;
+        while(temp.next!=null){
+            System.out.print("->"+temp.data);//display elements one by one
             temp=temp.next;
-
-            current.next=prev;
-            prev=current;
-            head=current;
-            //System.out.println(temp.data);
         }
+        if(temp.next==null){
+            System.out.println("->"+temp.data);//display last elements
+        }
+
     }
+
 
     public static void main(String[] args) {
         // write your code here
-        LinkedList linkedlist=new LinkedList();
+        LinkedList linkedlist=new LinkedList();//display linked list  object
+        linkedlist.addNode(56);//adding node
+        linkedlist.addNode(70);
+        linkedlist.addNode(50);
+        linkedlist.display();//call display function
+        System.out.println("enter elements to be insert:");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("enter first element:");
-        int data1=scanner.nextInt();
-        linkedlist.addNode(data1);
-        System.out.println("enter second element:");
-        int data2=scanner.nextInt();
-        linkedlist.addNode(data2);
-        System.out.println("enter third element:");
-        int data3=scanner.nextInt();
-        linkedlist.addNode(data3);
-        System.out.println("enter choice");
-        int ch= scanner.nextInt();
-        switch(ch) {
-            case 1:
-                linkedlist.reverse_display();
-                linkedlist.display();
-                break;
-            case 2:
-                System.out.println("exit");
-                break;
-        }
-
+        int x= scanner.nextInt();
+        linkedlist.addmiddle(x);//call middle insert function
+        System.out.println("linked list after adding element in middle:");
+        linkedlist.display();
 
     }
-
 }
